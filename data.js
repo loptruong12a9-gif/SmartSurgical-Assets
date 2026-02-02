dd82500969b3789d151999583375615e4026bd91
loptruong12a9-gif <loptruong12a9@gmail.com>
2026-02-02 22:15:06 +0700

    Cập nhật dữ liệu từ App (22:15:07 2/2/2026)

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "v1.3 PRO (Cập nhật 22:14:33 2/2/2026)";
+const APP_VERSION = "v1.3 PRO (Cập nhật 22:15:07 2/2/2026)";
 const kitDefinitions = [
     {
         "id": 1,
@@ -7489,15 +7489,13 @@ allKitsData["XƯƠNG CHI TRÊN"] = [
         "stt": 6,
         "name": "KÉO CẮT CHỈ",
         "code": "",
-        "quantity": 1,
-        "note": "CÙN"
+        "quantity": 1
     },
     {
         "stt": 7,
         "name": "KÉO MESK",
         "code": "",
-        "quantity": 1,
-        "note": "CÙN"
+        "quantity": 1
     },
     {
         "stt": 8,
@@ -7546,22 +7544,19 @@ allKitsData["XƯƠNG CHI TRÊN"] = [
         "stt": 15,
         "name": "ĐỤC LÒNG MÁNG NHỎ",
         "code": "FL136R",
-        "quantity": 1,
-        "note": "MẺ ĐẦU"
+        "quantity": 1
     },
     {
         "stt": 16,
         "name": "ĐỤC LÒNG MÁNG LỚN",
         "code": "FL574",
-        "quantity": 2,
-        "note": "MẺ"
+        "quantity": 2
     },
     {
         "stt": 17,
         "name": "ĐỤC THẲNG",
         "code": "FL524",
-        "quantity": 2,
-        "note": "MẺ"
+        "quantity": 2
     },
     {
         "stt": 18,
