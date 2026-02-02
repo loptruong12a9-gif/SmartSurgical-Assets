d901714287a433a3a153ac203020652b6e36c469
loptruong12a9-gif <loptruong12a9@gmail.com>
2026-02-02 23:25:29 +0700

    Cập nhật dữ liệu từ App (23:25:28 2/2/2026)

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "v1.3 PRO (Cập nhật 23:14:15 2/2/2026)";
+const APP_VERSION = "v1.3 PRO (Cập nhật 23:25:28 2/2/2026)";
 const kitDefinitions = [
     {
         "id": 1,
@@ -7489,7 +7489,8 @@ allKitsData["XƯƠNG CHI TRÊN"] = [
         "stt": 6,
         "name": "KÉO CẮT CHỈ",
         "code": "",
-        "quantity": 1
+        "quantity": 1,
+        "note": "CÙN"
     },
     {
         "stt": 7,
